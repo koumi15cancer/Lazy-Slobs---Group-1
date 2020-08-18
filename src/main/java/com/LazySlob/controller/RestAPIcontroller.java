3aa795c312d75e8588e52618c7317e947671ce5e
koumi15cancer <s3757278@rmit.edu.vn>
2020-08-18 13:01:06 +0700

    done

--- src/main/java/com/LazySlob/controller/RestAPIcontroller.java
+++ src/main/java/com/LazySlob/controller/RestAPIcontroller.java
@@ -16,7 +16,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
-
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @RequestMapping
 public class RestAPIcontroller {

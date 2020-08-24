6ab2d29dd0b5e0232b1e1caa501a0f0047121bb7
koumi15cancer <s3757278@rmit.edu.vn>
2020-08-25 03:48:01 +0700

    basic React connect with API for register/LogIn/Authorization/Authentication

--- src/main/resources/dataRole.sql
+++ src/main/resources/dataRole.sql
@@ -1,5 +1,5 @@
-CREATE SCHEMA IF NOT EXISTS restaurant;
-Use restaurant;
+
+Use  restaurant;
 INSERT INTO roles(name) VALUES('ROLE_USER');
 INSERT INTO roles(name) VALUES('ROLE_MODERATOR');
 INSERT INTO roles(name) VALUES('ROLE_ADMIN');

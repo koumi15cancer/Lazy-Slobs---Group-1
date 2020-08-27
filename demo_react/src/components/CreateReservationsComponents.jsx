93ddfdef159b6f3446b8824e55c3e3ff9e231b41
baotri <macbook@baotris-mbp.stusgs.rmit.edu.vn>
2020-08-27 12:57:50 +0700

    front-end

--- demo_react/src/components/CreateReservationsComponents.jsx
+++ demo_react/src/components/CreateReservationsComponents.jsx
@@ -90,47 +90,63 @@ class CreateReservationsComponents extends Component {
     }
     render() {
         return (
-            <div>
-                <div className ="container"></div>
-                <div className ="row">
-                    <div className = "card col-md-4 offset-md-4 offset-md-4">
-                        {
-                            this.getTitle()
-                        }
-                        <div className = "card-body">
-                            <form>
-                                <div className = "form-group">
-                                    <label> Customer Name: </label>
-                                    <input placeholder="Customer Name" name="customerName" className="form-control"
-                                           value={this.state.customerName} onChange={this.changeCustomerNameHandler}/>
-                                </div>
-                                <div className = "form-group">
-                                    <label> Email: </label>
-                                    <input placeholder=" Email" name="email" className="form-control"
-                                           value={this.state.email} onChange={this.changeEmailHandler}/>
-                                </div>
-
-                                <div className = "form-group">
-                                    <label> Quantity: </label>
-                                    <input placeholder="Quantity" name="quantity" className="form-control"
-                                           value={this.state.quantity} onChange={this.changeQuantityHandler}/>
-                                </div>
-                                <div className = "form-group">
-                                    <label> Description: </label>
-                                    <input placeholder="Description" name="description" className="form-control"
-                                           value={this.state.description} onChange={this.changeDescriptionHandler}/>
-                                </div>
-
-                                <button className="btn btn-success" onClick={this.saveOrUpdateReservation}>Save</button>
-                                <button className="btn btn-danger" onClick={this.cancel.bind(this)} style={{marginLeft: "10px"}}>Cancel</button>
-                            </form>
+            <span id="boxes-container">
+                <div class="box" id="time-open-box">
+                    <div id="border">
+                        <div>
+                            <h1 class="yeseva-one-font" style={{fontSize: "76px"}}>Time Open</h1>
+                            <h1 class="yeseva-one-font info-box-day">Weekday</h1>
+                            <h1 class="open-sans-condensed-light">7am-11am(Breakfast)</h1>
+                            <h1 class="open-sans-condensed-light">11am-12am(Lunch)</h1>
+                            <h1 class="open-sans-condensed-light">12am-10pm(Dinner)</h1>
+                            {"\n"}{"\n"}{"\n"} 
+                            <h1 class="yeseva-one-font info-box-day">Weekend</h1>
+                            <h1 class="open-sans-condensed-light">7am-12am(Breakfast-Lunch)</h1>
+                            <h1 class="open-sans-condensed-light">12am-10pm(Dinner)</h1>
                         </div>
                     </div>
-
                 </div>
-            </div>
+                <div class="box" id="form-box">
+                    <div style={{position: "relative", top: "30px"}}>
+                        <div id="content2">
+                            <h1 class="yeseva-one-font" style={{fontSize: "76px"}}>Reservation</h1>
+                               <form>
+                                    <div className = "form-group">
+                                            <label class="yeseva-one-font" style={{fontSize: "24px"}}> Customer Name: </label>
+                                            <input placeholder="Customer Name" name="customerName" className="form-control" 
+                                                value={this.state.customerName} onChange={this.changeCustomerNameHandler}/>
+                                        </div>
+                                        <div className = "form-group">
+                                            <label class="yeseva-one-font" style={{fontSize: "24px"}}> Email: </label>
+                                            <input placeholder=" Email" name="email" className="form-control" 
+                                                value={this.state.email} onChange={this.changeEmailHandler}/>
+                                        </div>
+                                        <div className = "form-group">
+                                            <label class="yeseva-one-font" style={{fontSize: "24px"}}> Phone Number: </label>
+                                            <input placeholder="Phone Number" name="phoneNumber" className="form-control" 
+                                                value={this.state.phoneNumber} onChange={this.changePhoneNumberHandler}/>
+                                        </div>
+                                        <div className = "form-group">
+                                            <label class="yeseva-one-font" style={{fontSize: "24px"}}> Quantity: </label>
+                                            <input placeholder="Quantity" name="quantity" className="form-control" 
+                                                value={this.state.quantity} onChange={this.changeQuantityHandler}/>
+                                        </div>
+                                        <div className = "form-group">
+                                            <label class="yeseva-one-font" style={{fontSize: "24px"}}> Description: </label>
+                                            <input placeholder="Description" name="description" className="form-control" 
+                                                value={this.state.description} onChange={this.changeDescriptionHandler}/>
+                                        </div>
+
+                                        <button id ="confirm-button2" onClick={this.saveOrUpdateReservation}>Save</button>
+                                        <button id ="confirm-button2" onClick={this.cancel.bind(this)} style={{marginLeft: "10px"}}>Cancel</button>
+                               </form>
+                           </div>
+                       </div>
+                   </div>
+            </span>
         );
     }
 }
+
 
 export default CreateReservationsComponents;

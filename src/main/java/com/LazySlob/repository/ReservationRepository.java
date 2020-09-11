b9c018232ac9b71bef8887832c4536509767116c
koumi15cancer <s3757278@rmit.edu.vn>
2020-09-11 07:35:26 +0700

    edit

--- src/main/java/com/LazySlob/repository/ReservationRepository.java
+++ src/main/java/com/LazySlob/repository/ReservationRepository.java
@@ -12,21 +12,4 @@ import org.springframework.data.repository.query.Param;
 // Repository for basic script for database
 @Repository
 public interface ReservationRepository extends JpaRepository<Reservation, Long> {
-//	@Query(value = "SELECT c FROM Reservation c"
-//			+ " WHERE c.customerName LIKE '%' || :keyword || '%'"
-//			+ " OR c.date LIKE '%' || :keyword || '%'"
-//			+ " OR c.description LIKE '%' || :keyword || '%'"
-//			+ " OR c.email LIKE '%' || :keyword || '%'"
-//			+ " OR c.quantity LIKE '%' || :keyword || '%'"
-//			+ " OR c.time LIKE '%' || :keyword || '%'"
-//			+ " OR c.user LIKE '%' || :keyword || '%'")
-	@Query(value = "SELECT COUNT(c) FROM Reservation c WHERE c.date LIKE '%' || :keyword || '%'")
-	public List<Reservation> getReservationCountFromDate(@Param("keyword") String keyword);
-
-	@Query(value = "SELECT c.time, COUNT(c.time) "
-	+ "FROM Reservation c "
-	+ "WHERE c.date LIKE '%' || :keyword || '%' "
-	+ "GROUP BY time "
-	+ "HAVING COUNT(c.time) >= 3")
-	public List<Reservation> searchTimeFromDate(@Param("keyword") String keyword);
 }
